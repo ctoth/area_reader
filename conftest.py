bd23e70b9da8333a423ad2fe776e66b3813604db
Christopher Toth <q.alpha@gmail.com>
2019-10-25 22:10:58 -0600

    Fix case sensitive tests

--- conftest.py
+++ conftest.py
@@ -2,9 +2,9 @@ import os
 
 def pytest_generate_tests(metafunc):
 	if "rom_path" in metafunc.fixturenames:
-		metafunc.parametrize("rom_path", files_in_directory('test/ROM'))
+		metafunc.parametrize("rom_path", files_in_directory('test/rom'))
 	if "merc_path" in metafunc.fixturenames:
-			metafunc.parametrize("merc_path", files_in_directory('test/Merc'))
+			metafunc.parametrize("merc_path", files_in_directory('test/merc'))
 
 def files_in_directory(directory):
 		for f in os.listdir(directory):
