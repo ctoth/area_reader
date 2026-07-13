67cf34cd27559d6d6459f0bd71dca4adb171bd89
Christopher Toth <q.alpha@gmail.com>
2026-07-13 12:54:39 -0600

    Resolve pytest parametrize deprecations

--- conftest.py
+++ conftest.py
@@ -2,9 +2,9 @@ import os
 
 def pytest_generate_tests(metafunc):
 	if "rom_path" in metafunc.fixturenames:
-		metafunc.parametrize("rom_path", files_in_directory('test/rom'))
+		metafunc.parametrize("rom_path", list(files_in_directory('test/rom')))
 	if "merc_path" in metafunc.fixturenames:
-			metafunc.parametrize("merc_path", files_in_directory('test/merc'))
+		metafunc.parametrize("merc_path", list(files_in_directory('test/merc')))
 
 def files_in_directory(directory):
 		for f in os.listdir(directory):
